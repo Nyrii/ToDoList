b92a1a2c7ea291705e47e5e34af54e91a6ea5695
Nyrandone Noboud-Inpeng <noboud_n@epitech.eu>
2017-01-15 00:08:38 +0100

    Toolbar in progress.

--- app/src/main/java/eu/epitech/todolist/MainActivity.java
+++ app/src/main/java/eu/epitech/todolist/MainActivity.java
@@ -4,7 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private ArrayList<Task> _toDoTasks = null;
     private ArrayList<Task> _doneTasks = null;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,12 +62,16 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        Collections.sort(_toDoTasks);
-        Collections.reverse(_toDoTasks);
-        Collections.sort(_doneTasks);
-        Collections.reverse(_doneTasks);
+        if (_toDoTasks != null && _toDoTasks.isEmpty()) {
+            Collections.sort(_toDoTasks);
+            Collections.reverse(_toDoTasks);
+        }
+        if (_doneTasks != null && _doneTasks.isEmpty()) {
+            Collections.sort(_doneTasks);
+            Collections.reverse(_doneTasks);
+        }
 
-        if (!_toDoTasks.isEmpty()) {
+        if (_toDoTasks != null && !_toDoTasks.isEmpty()) {
             final ListView lv = (ListView) findViewById(R.id.ListViewTasks);
             lv.setAdapter(new CustomBaseAdapter(this, _toDoTasks));
             lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -73,7 +83,93 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+
+
+        /*
+        Assigning view variables to thier respective view in xml
+        by findViewByID method
+         */
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        /*
+        Creating Adapter and setting that adapter to the viewPager
+        setSupportActionBar method takes the toolbar and sets it as
+        the default action bar thus making the toolbar work like a normal
+        action bar.
+         */
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(viewPagerAdapter);
+        setSupportActionBar(toolbar);
+
+        /*
+        TabLayout.newTab() method creates a tab view, Now a Tab view is not the view
+        which is below the tabs, its the tab itself.
+         */
+
+        final TabLayout.Tab home = tabLayout.newTab();
+        final TabLayout.Tab inbox = tabLayout.newTab();
+        final TabLayout.Tab star = tabLayout.newTab();
+
+        /*
+        Setting Title text for our tabs respectively
+         */
+
+        home.setText("Home");
+        inbox.setText("Inbox");
+        star.setText("Star");
+
+        /*
+        Adding the tab view to our tablayout at appropriate positions
+        As I want home at first position I am passing home and 0 as argument to
+        the tablayout and like wise for other tabs as well
+         */
+        tabLayout.addTab(home, 0);
+        tabLayout.addTab(inbox, 1);
+        tabLayout.addTab(star, 2);
+
+        /*
+        TabTextColor sets the color for the title of the tabs, passing a ColorStateList here makes
+        tab change colors in different situations such as selected, active, inactive etc
+
+        TabIndicatorColor sets the color for the indiactor below the tabs
+         */
+
+        tabLayout.setTabTextColors(ContextCompat.getColorStateList(this, R.color.tab_selector));
+        tabLayout.setSelectedTabIndicatorColor(ContextCompat.getColor(this, R.color.indicator));
+
+        /*
+        Adding a onPageChangeListener to the viewPager
+        1st we add the PageChangeListener and pass a TabLayoutPageChangeListener so that Tabs Selection
+        changes when a viewpager page changes.
+         */
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+
+
+//        initInstancesDrawer();
     }
+
+//    private void initInstancesDrawer() {
+//        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab One"));
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab Two"));
+//        tabLayout.addTab(tabLayout.newTab().setText("Tab Three"));
+//        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+//            @Override
+//            public void onTabSelected(TabLayout.Tab tab){
+//                int position = tab.getPosition();
+//            }
+//        });
+//    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/eu/epitech/todolist/ViewPagerAdapter.java
@@ -0,0 +1,27 @@
+package eu.epitech.todolist;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by noboud_n on 14/01/2017.
+ */
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return new TabFragment();    // Which Fragment should be dislpayed by the viewpager for the given position
+        // In my case we are showing up only one fragment in all the three tabs so we are
+        // not worrying about the position and just returning the TabFragment
+    }
+
+    @Override
+    public int getCount() {
+        return 3;           // As there are only 3 Tabs
+    }
+}
