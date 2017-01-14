53b63ee42584dceee94dbb31f3f696dc2c43e4c3
Nyrandone Noboud-Inpeng <noboud_n@epitech.eu>
2017-01-14 19:26:31 +0100

    HotFix on MainActivity

--- app/src/main/java/eu/epitech/todolist/MainActivity.java
+++ app/src/main/java/eu/epitech/todolist/MainActivity.java
@@ -1,13 +1,61 @@
 package eu.epitech.todolist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TODOLIST = "TDL_List";
+    private ArrayList<Task> _toDoTasks = new ArrayList<>();
+    private ArrayList<Task> _doneTasks = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences;
+        sharedPreferences = getSharedPreferences(TODOLIST, Context.MODE_PRIVATE);
+        String jsonToDoTasks = sharedPreferences.getString("Todo", "");
+
+        Gson gson = new Gson();
+        Task[] tasks = gson.fromJson(jsonToDoTasks, Task[].class);
+        if (tasks != null && tasks.length > 0) {
+            for (Task task : tasks) {
+                _toDoTasks.add(task);
+            }
+        }
+
+        String jsonDoneTasks = sharedPreferences.getString("Done", "");
+
+        tasks = gson.fromJson(jsonDoneTasks, Task[].class);
+        if (tasks != null) {
+            for (Task task : tasks) {
+                _doneTasks.add(task);
+            }
+        }
+
+        if (!_toDoTasks.isEmpty()) {
+            final ListView lv = (ListView) findViewById(R.id.ListViewTasks);
+            lv.setAdapter(new CustomBaseAdapter(this, _toDoTasks));
+            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+                    Object o = lv.getItemAtPosition(position);
+                    Task fullObject = (Task) o;
+                    Toast.makeText(getBaseContext(), "You have chosen: " + fullObject.getTitle(), Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 }
